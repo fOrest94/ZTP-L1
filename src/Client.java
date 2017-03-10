411c820b54ee3317c4489784a787f7e3b9f8b0be
fOrest94 <sewerdudek15@gmail.com>
2017-03-10 17:38:23 +0100

    checking each sensible sequence of inut string

--- src/Client.java
+++ src/Client.java
@@ -2,6 +2,7 @@
  * @author Seweryn Dudek
  * @version 1.0
  */
+
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
@@ -12,32 +13,59 @@ public class Client {
      * @param args
      */
     public static void main(String[] args) {
-        System.out.println(getLine(args[0], args[1]));
+        System.out.println("Linia : " + getLine(args[0], args[1]));
     }
 
-    /**
-     * @param url
-     * @param value
-     * @return numberOfLines
-     */
-    public static int getLine(String url, String value) {
+    private static int getLine(String url, String value) {
         int min = Integer.MAX_VALUE;
         int counter = 0;
         int minValue = 0;
-        Levenshtein levenshtein = new Levenshtein(url,value);
-
-       /* try (BufferedReader br = new BufferedReader(new FileReader(url.toString()))) {
+        value = value.trim();
+        try (BufferedReader br = new BufferedReader(new FileReader(url.toString()))) {
             String line;
             while ((line = br.readLine()) != null) {
                 ++counter;
-                int temp = levenshtein.getDist(line, value);
+                int temp = getLength(line, value);
                 if (min > temp) {
                     min = temp;
                     minValue = counter;
                 }
             }
-        } catch (IOException e) {e.printStackTrace();}
-        return minValue;*/
-       return levenshtein.getDisttance(url,value);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return minValue;
+    }
+
+    private static int getLength(String line, String word) {
+        Levenshtein levenshtein = new Levenshtein();
+        int min = levenshtein.getLevensthein(line, word);
+        String[] strings = word.split("\\s+");
+
+        if (strings.length > 1) {
+            min = Math.min(levenshtein.getLevensthein(line, twoStrCase(strings)), min);
+        }
+
+        if (strings.length > 2) {
+            min = Math.min(levenshtein.getLevensthein(line, threeStrCase(strings)), min);
+        }
+        return min;
+    }
+
+    private static String twoStrCase(String[] strings) {
+        String temp = strings[strings.length - 1].trim();
+        for (int i = 0; i < strings.length - 1; i++) {
+            temp += " " + strings[i].trim();
+        }
+        return temp;
+    }
+
+    private static String threeStrCase(String[] strings) {
+        String temp = "";
+        for (int i = 1; i < strings.length; i++) {
+            temp += strings[i].trim() + " ";
+        }
+        temp += strings[0].trim();
+        return temp;
     }
 }
--- src/Levenshtein.java
+++ src/Levenshtein.java
@@ -1,6 +1,3 @@
-import java.awt.*;
-import java.util.ArrayList;
-
 /**
  * @author Seweryn Dudek
  * @version 1.0
@@ -11,56 +8,8 @@ public class Levenshtein {
      * @param str2
      * @return numberOfLines
      */
-    private ArrayList<String> strings1;
-    private ArrayList<String> strings2;
-    private String str1;
-    private String str2;
-    private int lenght1;
-    private int lenght2;
+    public int getLevensthein(String str1, String str2) {
 
-    public Levenshtein(String str1, String str2) {
-        this.str1 = str1;
-        this.str2 = str2;
-        this.lenght1 = str1.length();
-        this.lenght2 = str2.length();
-    }
-
-    public int getDisttance(String str1, String str2) {
-        cleanSpaces();
-        this.strings1 = checkQuantity(str1);
-        this.strings2 = checkQuantity(str2);
-
-        if (strings1.size() == 2 && strings2.size() == strings1.size()) {
-            if (strings1.get(0).equals(strings2.get(0)) && strings1.get(1).equals(strings2.get(1)) ||
-                    strings1.get(0).equals(strings2.get(1)) && strings1.get(1).equals(strings2.get(0))) {
-                return 0;
-            } else if (strings1.get(0).equals(strings2.get(0)) && !strings1.get(1).equals(strings2.get(1))) {
-                return getLev(strings1.get(1), strings2.get(1));
-            } else if (strings1.get(1).equals(strings2.get(1)) && !strings1.get(0).equals(strings2.get(0))) {
-                return getLev(strings1.get(0), strings2.get(0));
-            } else if (strings1.get(0).equals(strings2.get(1)) && !strings1.get(1).equals(strings2.get(0))) {
-                return getLev(strings1.get(1), strings2.get(0));
-            } else if (strings1.get(1).equals(strings2.get(0)) && !strings1.get(0).equals(strings2.get(1))) {
-                return getLev(strings1.get(0), strings2.get(1));
-            } else if (strings1.get(0).equals(strings2.get(0)) && strings1.get(1).equals(strings2.get(1)) ||
-                    strings1.get(0).equals(strings2.get(1)) && strings1.get(1).equals(strings2.get(0))) {
-                return getLev(strings1.get(1), strings2.get(1)) + getLev(strings1.get(0), strings2.get(0));
-            }
-
-        } else if (strings1.size() == 2 && strings2.size() == 3) {
-
-
-        } else if (strings1.size() == 3 && strings2.size() == 2) {
-
-        } else if (strings1.size() == 3 && strings2.size() == strings1.size()) {
-
-            System.out.println(strings1.toString() + " haha " + strings2.toString());
-
-            return 0;
-        }
-
-    public int getLev(String str1, String str2) {
-        System.out.println(str1 + " hah " + str2);
         int l1 = str1.length();
         int l2 = str2.length();
         int cost;
@@ -75,38 +24,7 @@ public class Levenshtein {
         return ar[l1 - 1][l2 - 1];
     }
 
-    public void cleanSpaces() {
-        str1 = str1.trim();
-        str2 = str2.trim();
-    }
-
-    public ArrayList<String> checkQuantity(String str) {
-        int counter = 1;
-        int index = 0;
-        String temp = new String();
-        ArrayList<String> strings = new ArrayList<>();
-        for (int i = 0; i < str.length(); i++) {
-            if (str.charAt(i) != ' ') {
-                temp = temp + str.charAt(i);
-                counter = 0;
-                if (i == str.length() - 1)
-                    strings.add(index, temp);
-            } else if (str.charAt(i) == ' ' && counter == 0) {
-                strings.add(index++, temp);
-                temp = "";
-                counter++;
-            }
-        }
-        return strings;
-    }
-
-    /**
-     * @param array
-     * @param l1
-     * @param l2
-     * @return
-     */
-    public int[][] fillTable(int[][] array, int l1, int l2) {
+    private int[][] fillTable(int[][] array, int l1, int l2) {
         for (int i = 0; i < l1 + 1; i++) {
             array[i][0] = i;
         }
@@ -115,6 +33,4 @@ public class Levenshtein {
         }
         return array;
     }
-
-
 }
